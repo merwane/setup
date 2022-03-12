5968892f4ea8faa1840dc6cdcedbfc9ac4844f21
Merwane Drai <merwanedr@gmail.com>
2022-03-12 13:51:31 +0000

    ffs

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 add-apt-repository ppa:jonathonf/vim
 apt-get update
 
-curl -fsSL https://raw.github.com/merwane/dotfiles/master/.zshrc > .zshrc
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -fsSL https://raw.github.com/merwane/dotfiles/master/.zshrc > .zshrc
 curl -fsSL https://raw.github.com/merwane/dotfiles/master/.vimrc > .vimrc
 curl -fsSL https://raw.github.com/merwane/dotfiles/master/.tmux.conf > .tmux.conf
 curl -fsSL https://raw.github.com/merwane/dotfiles/master/.vim/autoload/plug.vim > .vim/autoload/plug.vim
