15b73ae7c51425c04e7c33622e09764086a8d71c
Merwane Drai <merwanedr@gmail.com>
2022-03-12 13:47:58 +0000

    zsh first

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 add-apt-repository ppa:jonathonf/vim
 apt-get update
 
-
 curl -fsSL https://raw.github.com/merwane/dotfiles/master/.zshrc > .zshrc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -fsSL https://raw.github.com/merwane/dotfiles/master/.vimrc > .vimrc
 curl -fsSL https://raw.github.com/merwane/dotfiles/master/.tmux.conf > .tmux.conf
 curl -fsSL https://raw.github.com/merwane/dotfiles/master/.vim/autoload/plug.vim > .vim/autoload/plug.vim
 
 apt install -y vim
 apt-get install -y zsh
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
