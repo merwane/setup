c12e715b4151ba0630c9bc8e591bd7d159c1d4cc
Merwane Drai <merwanedr@gmail.com>
2022-03-12 13:23:50 +0000

    add README

---
+++ README.md
@@ -0,0 +1,5 @@
+## Install
+
+```bash
+sh -c "$(curl -fsSL https://raw.github.com/merwane/dotfiles/master/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ add-apt-repository ppa:jonathonf/vim
 apt-get update
 
 git clone https://github.com/merwane/dotfiles/.vimrc ./
-git clone https://github.com/merwane/dotfiles/.zshrc ./
 git clone https://github.com/merwane/dotfiles/.tmux.conf ./
 git clone https://github.com/merwane/dotfiles/.vim ./
 
 apt install -y vim
 apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/merwane/dotfiles/.zshrc ./
