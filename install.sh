2f258c43bb84d8dd967689d93b2268ca646b7ff0
Merwane Drai <merwanedr@gmail.com>
2022-03-12 13:44:30 +0000

    curl not clone

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 add-apt-repository ppa:jonathonf/vim
 apt-get update
 
-git clone https://github.com/merwane/dotfiles/.vimrc ./
-git clone https://github.com/merwane/dotfiles/.tmux.conf ./
-git clone https://github.com/merwane/dotfiles/.vim/ ./
+
+curl -fsSL https://raw.github.com/merwane/dotfiles/master/.zshrc > .zshrc
+curl -fsSL https://raw.github.com/merwane/dotfiles/master/.vimrc > .vimrc
+curl -fsSL https://raw.github.com/merwane/dotfiles/master/.tmux.conf > .tmux.conf
+curl -fsSL https://raw.github.com/merwane/dotfiles/master/.vim/autoload/plug.vim > .vim/autoload/plug.vim
 
 apt install -y vim
 apt-get install -y zsh
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/merwane/dotfiles/.zshrc ./
