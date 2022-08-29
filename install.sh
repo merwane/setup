61246f3dae86a531ea543ca5405cf12ff5eaf90f
Merwane Drai <merwanedr@gmail.com>
2022-08-29 01:46:41 +0100

    changes

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 add-apt-repository ppa:jonathonf/vim
+add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 
+apt install python3.7
 apt install -y vim
 apt-get install -y zsh
+apt-get install -y tmux
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -13,5 +16,5 @@ curl -fsSL https://raw.github.com/merwane/dotfiles/master/.tmux.conf > .tmux.con
 mkdir .vim
 mkdir .vim/autoload
 
-curl -fsSL https://raw.github.com/merwane/dotfiles/master/.vim/autoload/plug.vim > .vim/autoload/plug.vim
+curl -fsSL https://raw.github.com/merwane/dotfiles/master/plug.vim > .vim/autoload/plug.vim
 
